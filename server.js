8f8c3bf9121478d8a0372d3bf9d101afa0caef86
Bala1998 <30731547+Bala1998@users.noreply.github.com>
2017-08-17 23:02:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/Article-one',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
 });
 app.get('/Article-two',function(req,res){
-    res.send('Article two will be requested and sent here');
+     res.sendFile(path.join(__dirname, 'ui', 'Article-one.html'));
 });
 app.get('/Article-three',function(req,res){
     res.send('Article three will be requested and sent here');
